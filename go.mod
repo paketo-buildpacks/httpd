58b3f1cae78806fd49cef1a5e487cbb35458f018
Danny Joyce <djoyce@pivotal.io>
2019-02-04 15:16:29 -0500

    Bump dagger and fix int tests [#163595064]
    Co-authored-by: David Freilich <dfreilich@pivotal.io>

--- go.mod
+++ go.mod
@@ -1,11 +1,14 @@
 module github.com/cloudfoundry/httpd-cnb
 
 require (
-	github.com/buildpack/libbuildpack v1.9.0
-	github.com/cloudfoundry/dagger v0.0.0-20190107214837-91916b900aea
-	github.com/cloudfoundry/libcfbuildpack v1.37.0
-	github.com/cloudfoundry/php-cnb v0.0.0-20190124222934-6a3af4fef5bd
+	github.com/buildpack/libbuildpack v1.10.0
+	github.com/cloudfoundry/dagger v0.0.0-20190204200826-d194e96e09ab
+	github.com/cloudfoundry/libcfbuildpack v1.39.0
+	github.com/cloudfoundry/php-cnb v0.0.1
+	github.com/kr/pty v1.1.3 // indirect
+	github.com/mattn/go-colorable v0.1.0 // indirect
 	github.com/onsi/gomega v1.4.3
 	github.com/sclevine/spec v1.2.0
+	golang.org/x/sys v0.0.0-20190204103248-980327fe3c65 // indirect
 	gopkg.in/yaml.v2 v2.2.2
 )
--- integration/integration_test.go
+++ integration/integration_test.go
@@ -22,8 +22,6 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/buildpack/libbuildpack/buildpack"
-
 	"github.com/cloudfoundry/dagger"
 
 	"github.com/sclevine/spec"
@@ -43,31 +41,10 @@ func testIntegration(t *testing.T, when spec.G, it spec.S) {
 
 	when("push simple app", func() {
 		it("serves up staticfile", func() {
-
 			uri, err := dagger.PackageBuildpack()
 			Expect(err).ToNot(HaveOccurred())
 
-			//set up buildermetadata for 'old way' of using pack in dagger
-			builderMetadata := dagger.BuilderMetadata{
-				Buildpacks: []dagger.Buildpack{
-					{
-						ID:  "org.cloudfoundry.buildpacks.httpd",
-						URI: uri,
-					},
-				},
-				Groups: []dagger.Group{
-					{
-						[]buildpack.Info{
-							{
-								ID:      "org.cloudfoundry.buildpacks.httpd",
-								Version: "0.0.1",
-							},
-						},
-					},
-				},
-			}
-
-			app, err := dagger.Pack(filepath.Join("fixtures", "simple_app"), builderMetadata, dagger.CFLINUXFS3)
+			app, err := dagger.PackBuild(filepath.Join("fixtures", "simple_app"), uri)
 			Expect(err).ToNot(HaveOccurred())
 
 			app.SetHealthCheck("", "3s", "1s")
@@ -76,20 +53,20 @@ func testIntegration(t *testing.T, when spec.G, it spec.S) {
 			err = app.Start()
 			if err != nil {
 				_, err = fmt.Fprintf(os.Stderr, "App failed to start: %v\n", err)
-				containerID, imageName, volumeIDs, err := app.ContainerInfo()
+				containerID, imageName, volumeIDs, err := app.Info()
 				Expect(err).NotTo(HaveOccurred())
 				fmt.Printf("ContainerID: %s\nImage Name: %s\nAll leftover cached volumes: %v\n", containerID, imageName, volumeIDs)
 
-				containerLogs, err := app.ContainerLogs()
+				containerLogs, err := app.Logs()
 				Expect(err).NotTo(HaveOccurred())
 				fmt.Printf("Container Logs:\n %s\n", containerLogs)
 				t.FailNow()
 			}
 
-			err = app.HTTPGet("/index.html")
+			_, _, err = app.HTTPGet("/index.html")
 			Expect(err).ToNot(HaveOccurred())
 
-			Expect(app.Destroy()).To(Succeed()) //Only destroy app if the test passed to leave artifacts to debug
+			Expect(app.Destroy()).To(Succeed())
 		})
 	})
 }
