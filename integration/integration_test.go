1676213c48cc4145c9a5184bec0b27c9e2ab6aa0
Don Nguyen <dnguyen@pivotal.io>
2019-02-19 14:21:05 -0500

    convert scripts to native vendoring
    [#163874965]
    
    Co-authored-by: Daniel Thornton <dthornton@pivotal.io>

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -37,6 +37,8 @@ func TestIntegration(t *testing.T) {
 func testIntegration(t *testing.T, when spec.G, it spec.S) {
 	it.Before(func() {
 		RegisterTestingT(t)
+		err := dagger.BuildCFLinuxFS3()
+		Expect(err).ToNot(HaveOccurred())
 	})
 
 	when("push simple app", func() {
