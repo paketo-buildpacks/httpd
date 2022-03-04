c0e1e2d668e3f12aacf8a6b43f713ac93d19db93
TisVictress <vhenry@vmware.com>
2022-03-04 14:34:48 -0500

    Adds check for BP_LIVE_RELOAD_ENABLED (#290)

--- integration/simple_app_test.go
+++ integration/simple_app_test.go
@@ -14,7 +14,7 @@ import (
 	. "github.com/paketo-buildpacks/occam/matchers"
 )
 
-func testSimpleApp(t *testing.T, when spec.G, it spec.S) {
+func testSimpleApp(t *testing.T, context spec.G, it spec.S) {
 	var (
 		Expect     = NewWithT(t).Expect
 		Eventually = NewWithT(t).Eventually
@@ -68,5 +68,61 @@ func testSimpleApp(t *testing.T, when spec.G, it spec.S) {
 		response, err := http.Get(fmt.Sprintf("http://localhost:%s", container.HostPort("8080")))
 		Expect(err).NotTo(HaveOccurred())
 		Expect(response.StatusCode).To(Equal(http.StatusOK))
+	})
+
+	context("when BP_LIVE_RELOAD_ENABLED=true", func() {
+		var noReloadContainer occam.Container
+
+		it.Before(func() {
+			var err error
+			source, err = occam.Source(filepath.Join("testdata", "simple_app"))
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		it.After(func() {
+			Expect(docker.Container.Remove.Execute(noReloadContainer.ID)).To(Succeed())
+		})
+
+		it("adds a reloadable process type as the default process", func() {
+			var err error
+			var logs fmt.Stringer
+
+			image, logs, err = pack.Build.
+				WithBuildpacks(
+					watchexecBuildpack,
+					httpdBuildpack,
+				).
+				WithPullPolicy("never").
+				WithEnv(map[string]string{
+					"BP_LIVE_RELOAD_ENABLED": "true",
+				}).
+				Execute(name, source)
+			Expect(err).NotTo(HaveOccurred())
+
+			container, err = docker.Container.Run.
+				WithEnv(map[string]string{"PORT": "8080"}).
+				WithPublish("8080").
+				WithPublishAll().
+				Execute(image.ID)
+			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(container).Should(BeAvailable())
+
+			response, err := http.Get(fmt.Sprintf("http://localhost:%s", container.HostPort("8080")))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(response.StatusCode).To(Equal(http.StatusOK))
+
+			noReloadContainer, err = docker.Container.Run.
+				WithEnv(map[string]string{"PORT": "8080"}).
+				WithPublish("8080").
+				WithPublishAll().
+				WithEntrypoint("no-reload").
+				Execute(image.ID)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(logs).To(ContainLines("  Assigning launch processes:"))
+			Expect(logs).To(ContainLines("    web (default): watchexec --restart --watch /workspace --shell none -- httpd -f /workspace/httpd.conf -k start -DFOREGROUND"))
+			Expect(logs).To(ContainLines("    no-reload:     httpd -f /workspace/httpd.conf -k start -DFOREGROUND"))
+		})
 	})
 }
